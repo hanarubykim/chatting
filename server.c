b015d330575cffbe7dc36a8845399e1ff24dda2a
Christy Guan <cguan00@stuy.edu>
2020-01-05 16:57:14 -0500

    testing pqueue

--- server.c
+++ server.c
@@ -4,18 +4,38 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include "pqueue.c"
 
 int main(){
-  char name[256];
-  printf("WELCOME TO CHAT ROOM\n");
-  printf("Enter your name: ");
-  fgets(name, 256, stdin);
-  name[strlen(name) - 1] = 0;
 
-  printf("Welcome, %s\n", name);
+  Node* pq = newNode("m1", 1);
+    push(&pq, "m2", 2);
+    push(&pq, "m3", 3);
+    push(&pq, "m4", 0);
 
-  
+    while (!isEmpty(&pq)) {
+      printf("%s \n", peek(&pq));
+      pop(&pq);
+    }
+
+
+  // char name[256];
+  // printf("WELCOME TO CHAT ROOM\n");
+  // printf("Enter your name: ");
+  // fgets(name, 256, stdin);
+  // name[strlen(name) - 1] = 0;
+  //
+  // printf("Welcome, %s\n\n", name);
+  //
+  // char message[256];
+  //
+  // printf("Please enter a message: ");
+  // fgets(message, 256, stdin);
+  // message[strlen(message) - 1] = 0;
+  // printf("Message you just entered: %s\n", message);
+
+
+
 
   return 0;
 }
