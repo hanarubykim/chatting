04f6f5c9012f7e3b211c3c42e06404332064fca1
Christy Guan <cguan00@stuy.edu>
2020-01-18 16:42:42 -0500

    working on printing line function

--- select_client.c
+++ select_client.c
@@ -40,13 +40,6 @@ void choosingColor(){
 
   int chosen = atoi(colorNum);
 
-  // chosenClientColor = coloring[chosen];
-  // char * coloredName = calloc(256, sizeof(char));
-  // coloredName = strcat("\033[4", chosenClientColor);
-  // coloredName = strcat(coloredName, clientName);
-  // coloredName = strcat(coloredName, "\033[1");
-  // coloredName = strcat(coloredName, coloring[6]);
-
 
   chosenClientColor = calloc(256, sizeof(char));
   strcpy(chosenClientColor, coloring[chosen]);
@@ -57,13 +50,12 @@ void choosingColor(){
 
   strcat(coloredName, chosenClientColor);
 
+  strcat(coloredName, clientName);
+
   strcat(coloredName, "\033[1");
 
-  //clientName = coloredName;
   strcpy(clientName, coloredName);
-  printf("%s", clientName);
-  //return clientColoring[chosen];
-  //printf("\033%sHELLO\033%s\n", clientColoring[chosen], clientColoring[7]);
+
 }
 
 int booting(){
@@ -85,6 +77,7 @@ int booting(){
   if(strstr(decide, "yes")){
     choosingColor();
     printf("GETS UP TO HERE\n");
+    printf("HIIIII%s", clientName); //for testing purposes
   }
   else{
     printf("Alrighty then! That's fine too. :) \n");
@@ -116,12 +109,23 @@ char * timeStamp(){
 void printingLine(int server_socket, char * message){
   char * timey = timeStamp();
   char * chatLine = calloc(256, sizeof(char));
+
+  //THIS IS DONE BY CHRISTY.
+  // strcpy(chatLine, clientName);//name
+  // strcat(chatLine, timey);//followed by time
+  // strcat(chatLine, message);// followed by message
+  // strcat(chatLine, "\n");
+
+  //THIS WAS DONE BY HANA
   chatLine = strcat(clientName, timey);
   chatLine = strcat(chatLine, message);
   chatLine = strcat(chatLine, "\n");
 
   write(server_socket, message, sizeof(message));
   read(server_socket, message, sizeof(message));
+
+
+
 }
 
 void channel(char * ip, char * portNum){
@@ -178,28 +182,8 @@ void channel(char * ip, char * portNum){
     }
   }
 
-}// printf("CHOOSING USERNAME COLOR... HERE ARE YOUR OPTIONS\n");
-  // printf("(1) RED\n(2) GREEN\n(3) YELLOW\n(4) BLUE\n(5) MAGENTA\n(6) CYAN\n(7) WHITE\n\n");
-  // printf("PLEASE ENTER THE NUMBER OF YOUR DESIRED USERNAME COLOR: ");
-  //
-  // char colorNum[256];
-  // fgets(colorNum, 256, stdin);
-  // colorNum[strlen(colorNum) - 1] = '\0';
-  //
-  // int chosen = atoi(colorNum);
-  //
-  // chosenClientColor = coloring[chosen];
-  // char * coloredName = calloc(256, sizeof(char));
-  // coloredName = strcat("\033[4", chosenClientColor);
-  // coloredName = strcat(coloredName, clientName);
-  // coloredName = strcat(coloredName, "\033[1");
-  // coloredName = strcat(coloredName, coloring[7]);
-  //
-  // //clientName = coloredName;
-  // strcpy(clientName, coloredName);
-  // printf("%s", clientName);
-  // //return clientColoring[chosen];
-  // //printf("\033%sHELLO\033%s\n", clientColoring[chosen], clientColoring[7]);
+}
+
 
 int main(int argc, char **argv){
   // colors();
@@ -228,62 +212,3 @@ int main(int argc, char **argv){
 
   return 0;
 }
-
-
-
-
-// int main(int argc, char **argv) {
-//
-//   int server_socket;
-//   char buffer[BUFFER_SIZE];
-//
-//   fd_set read_fds;
-//
-//   if (argc == 2)
-//     server_socket = client_setup( argv[1]);
-//   else
-//     server_socket = client_setup( TEST_IP );
-//
-//   while (1) {
-//
-//     printf("enter data: ");
-//     //the above printf does not have \n
-//     //flush the buffer to immediately print# forking: client fserver
-//     fflush(stdout);
-//
-//     //select() modifies read_fds
-// //     //we must reset it at each iterationint main(int argc, char **argv) {
-//   if (argc == 2)
-//     server_socket = channel( argv[1], PORT);
-//   else
-//     server_socket = channel( TEST_IP, PORT );
-//   }
-//     FD_ZERO(&read_fds);
-//     FD_SET(STDIN_FILENO, &read_fds); //add stdin to fd set
-//     FD_SET(server_socket, &read_fds); //add socket to fd set
-//
-//     //select will block until either fd is ready
-//     select(server_socket + 1, &read_fds, NULL, NULL, NULL);
-//
-//     if (FD_ISSET(STDIN_FILENO, &read_fds)) {
-//       fgets(buffer, sizeof(buffer), stdin);
-//       *strchr(buffer, '\n') = 0;
-//       write(server_socket, buffer, sizeof(buffer));
-//       read(server_socket, buffer, sizeof(buffer));
-//       printf("received: [%s]\n", buffer);
-//     }//end stdin select
-//
-//     //currently the server is not set up to
-//     //send messages to all the clients, but
-//     //this would allow for broadcast messages
-//     if (FD_ISSET(server_socket, &read_fds)) {
-//       read(server_socket, buffer, sizeof(buffer));
-//       printf("[SERVER BROADCAST] [%s]\n", buffer);
-//       printf("enter data: ");
-//       //the above printf does not have \n
-//       //flush the buffer to immediately print
-//       fflush(stdout);
-//     }//end socket select
-//
-//   }//end loop
-// }
